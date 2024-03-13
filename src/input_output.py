56ef835076cd0cb4a9ce2c8e3e3bf37a7effe888
Allen <alau002@citymail.cuny.edu>
2024-03-13 13:50:05 -0400

    input_output class, implementation to retrieve data from source

---
+++ src/input_output.py
@@ -0,0 +1,114 @@
+#libraries 
+import requests 
+import logging 
+import json 
+
+class input_output():
+    """
+    Implementation for retrieval of data from source and 
+    load data into cloud storage. 
+    """
+
+    def __init__(self):
+        """
+        Generator function
+        """
+        self.api_key = ''
+
+    def get(self,url):
+        """
+        Request data from source using defined API endpoint, API dev key, and format 
+
+        Args:
+            url (string): API endpoint
+
+        Returns:
+            list: list of data 
+        """
+        try:
+            #request event information 
+            r = requests.get(url)
+            #retrieve data and map to dictionary
+            data = json.loads(r.text)
+        except Exception as e:
+            logging.error(e)
+        return data
+
+    def GETEVENTS(self,format='json'):
+        """
+        Retrieve Events data from source
+
+        Args:
+            format (str): str or xml 
+
+        Returns:
+            list: list of events data
+        """
+        return self.get(f'https://511ny.org/api/getevents?key={self.api_key}&format={format}')
+    
+    def GETROADWAYS(self,format='json'):
+        """
+        Retrieve roadways data from source
+
+        Args:
+            format (str): str or xml 
+
+        Returns:
+            list: list of roadways data
+        """
+        return self.get(f'https://511ny.org/api/getroadways?key={self.api_key}&format={format}')
+    
+    def GETCAMERAS(self,format='json'):
+        """
+        Retrieve camera data from source
+
+        Args:
+            format (str): str or xml 
+
+        Returns:
+            list: list of camera data
+        """
+        return self.get(f'https://511ny.org/api/getcameras?key={self.api_key}&format={format}')
+    
+    def GETMESSAGESIGNS(self,format='json'):
+        """
+        Retrieve message sign data from source
+
+        Args:
+            format (str): str or xml 
+
+        Returns:
+            list: list of message sign data
+        """
+        return self.get(f'https://511ny.org/api/getmessagesigns?key={self.api_key}&format={format}')
+    
+    def GETALERTS(self,format='json'):
+        """
+        Retrieve alerts sign data from source
+
+        Args:
+            format (str): str or xml 
+
+        Returns:
+            list: list of alerts data
+        """
+        return self.get(f'https://511ny.org/api/getalerts?key={self.api_key}&format={format}')
+    
+
+    def GETWINTERROADCONDITIONS(self,format='json'):
+        """
+        Retrieve winter road conditions sign data from source
+
+        Args:
+            format (str): str or xml 
+
+        Returns:
+            list: list of winter road conditions data
+        """
+        return self.get(f'https://511ny.org/api/getwinterroadconditions?key={self.api_key}&format={format}')
+
+if __name__ == '__main__':
+    # test = input_output()
+    # data = test.GETEVENTS()
+    # print(data[0])
+    pass
