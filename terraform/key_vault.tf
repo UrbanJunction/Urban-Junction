b4595e113ffa265a5cc12ff4785aa38571806a4e
Allen <alau002@citymail.cuny.edu>
2024-03-15 17:54:41 -0400

    permissions for kv

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -11,6 +11,21 @@ resource "azurerm_key_vault" "urban_junction_key_vault" {
     purge_protection_enabled = false 
     sku_name = "standard"
     tenant_id = data.azurerm_client_config.current.tenant_id
+
+    access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+      "Set",
+      "Delete"
+    ]
+  }
 }
 
 resource "azurerm_key_vault_secret" "ny511_secret" {
